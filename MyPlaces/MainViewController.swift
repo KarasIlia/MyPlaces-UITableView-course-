03ca236574394f0b87939ce1f79754e213cc8932
Ilia <terpariz@mail.ru>
2019-08-17 21:37:03 +0300

    MainViewController navigation bar buttons New place adding screen Keyboard hiding after editing end Separation lines hided Segue to from adding screen to mainViewController

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class MainViewController: UITableViewController {
-    
+    /*
     let restaurantNames = ["McDonald's", "KFC", "ML", "Papa Johns", "Molly Malone"]
+    */
+    
+    let places = Place.getPlaces()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,27 +22,22 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
-//        cell.typeLabel.text =
-//        cell.locationLabel.text = 
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
+        
         return cell
-    }
-    
-    // MARK: - Table view delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
     }
     
     // MARK: - Navigation
@@ -47,4 +45,6 @@ class MainViewController: UITableViewController {
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        <#code#>
 //    }
+    
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 }
---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Илья Карась on 17/08/2019.
+//  Copyright © 2019 Ilia Karas. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView() // Убираем лишние разделительные линии таблицы
+    }
+
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK: - Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
